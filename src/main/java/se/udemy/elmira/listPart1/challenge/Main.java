d1cb3840625735b7ec808672dc64772147b42cca
EELMMAD <elmiramadadi@gmail.com>
2022-05-29 22:54:36 +0200

    Add more methods

---
+++ src/main/java/se/udemy/elmira/listPart1/challenge/Main.java
@@ -0,0 +1,14 @@
+package se.udemy.elmira.listPart1.challenge;
+
+public class Main {
+    //Create a program that implements a simple mobile phone with the following capabilities:
+    //Able to store, modify, remove and query contact names.
+    //You will want to create a separate class for contacts(name and phone number).
+    //Create a master class (MobilPhone) that holds the ArrayList of Contacts.
+    //The MobilPhone class has the functionality listed above.
+    //Add a menu of options that are available.
+    //Options: Quit, print list of contacts, add new contacts, update existing contact, remove contact
+    //and search/find contact.
+    //When adding or updating be sure to check if the contact already exists (use name)
+    //Be sure not to expose the inner working of the Arraylist to mobiw
+}
